1ae1dd7a7f9492f2c6545c6e98f06e1a24f1cf22
Muhammed Cindioglu <mhdcindioglu@gmail.com>
2020-02-25 02:39:55 +0300

    24-02-2020

---
+++ Inputs/AutoComplete/McInputAutoComplete.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.Blazor
+{
+    public class McInputAutoCompleteBase : InputBase<string>
+    {
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string InputCssClass { get; set; }
+        [Parameter] public Dictionary<string, string> Items { get; set; }
+        [Parameter] public Expression<Func<string>> ValidationFor { get; set; }
+        [Parameter] public EventCallback<object> OnItemSelected { get; set; }
+        [Parameter] public EventCallback<string> OnTextChanged { get; set; }
+
+        protected object SelectedId { get; set; }
+
+        protected override bool TryParseValueFromString(string value, out string result, out string validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+
+        protected async Task OnTextChangedVoid(string value)
+        {
+            await OnTextChanged.InvokeAsync(value);
+        }
+    }
+}
